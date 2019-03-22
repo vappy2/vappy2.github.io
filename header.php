fda2dcff65e6cc4d783fc41f8441c005cacc39f2
vappy2 <valentine.appy@outlook.fr>
2019-03-22 09:39:32 +0100

    Intégration de bootstrap

--- header.php
+++ header.php
@@ -1,3 +1,10 @@
 <!DOCTYPE html>
+
+<!-- encodage utf-8 -->
   <meta http-equiv="content-type" contant="text/html charset=UTF-8" />
-  
+
+  <!-- Intégration de boostrap 4 -->
+  <script src="bootstrap/bootstrap.min.js"></script>
+  <script src="bootstrap/jquery.min.js"></script>
+  <script scr="bootstrap/popper.min.js"></script>
+  <link rel="stylesheet" href="bootstrap/bootstrap.min.css" />
