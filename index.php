e6d8054cbdc87e6d37c4ca019f876b1ce0d12862
vappy2 <valentine.appy@outlook.fr>
2019-03-22 09:46:45 +0100

    Modification index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<DOCTYPE html>
+  <html lang=fr>
+  <head>
+
+    <?php require_once("header.php"); ?>
+    <title>
+      Accueil
+    </title>
+
+  </head>
+
+  <body>
+    <div class="jumbotron text-center" style="background-color:pink;">
+      <h1>Portfolio</h1>
+      <h4> APPY Valentine, BTS SIO option SLAM </h4>
+    </div>
+
+    <nav class="navbar navbar-default" id="navbar">
+
+
+  </body>
+
+  </html>
